0291e7dfe1de77484265640bee25c3216e81772b
viniciochavess <viniciochavess@gmail.com>
2026-07-06 16:55:28 -0300

    obs: aula 37 ddd

--- src/infrastructure/db/sequelize/model/productModel.ts
+++ src/infrastructure/db/sequelize/model/productModel.ts
@@ -1,7 +1,18 @@
-import { Model, Table } from 'sequelize-typescript';
+import { Column, Model, PrimaryKey, Table } from 'sequelize-typescript';
 
 @Table({
     tableName: 'products',
     timestamps: false,
 })
-export default class ProductModel extends Model {}
+export default class ProductModel extends Model {
+
+    @PrimaryKey()
+    @Column()
+    declare id: string;
+
+    @Column({allowNull: false})
+    declare name: string;
+
+    @Column({allowNull: false})
+    declare price: number;
+}
